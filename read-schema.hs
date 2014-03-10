36cabb8350d829721f2f07929fdff46e0cc24d97
Finlay Thompson <finlay@dragonfly.co.nz>
2014-03-10 14:21:13 +1300

    Recursion !

--- read-schema.hs
+++ read-schema.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 import System.IO (stdin)
-import Data.Text as T
+import Data.Monoid ((<>))
+import Data.Maybe (fromJust, catMaybes)
+
+import Data.Text as T hiding (map)
 import Data.Text.IO as T
 import Data.Conduit (($$))
 import Data.Conduit.Binary (sourceHandle)
@@ -12,7 +15,21 @@ main = do
     T.putStrLn $ showdoc doc
 
 showdoc :: X.Document -> T.Text
-showdoc doc = 
-    let cursor = fromDocument doc
-    in  T.concat $ cursor $/ element "xs:element" &/ attribute "name"
+showdoc doc = showElement 0 $ documentRoot doc
 
+rep i = Prelude.take i . Prelude.repeat
+
+type Depth = Int
+showElement :: Depth -> Element -> T.Text
+showElement d el = 
+    let te = showName $ elementName el
+        selectElements (NodeElement el) = Just el
+        selectElements _ = Nothing
+        children = catMaybes $ map selectElements $ elementNodes el
+        prefix :: Int -> Text -> Text
+        prefix d txt = T.concat $ rep d "    " ++ [txt]
+        ch = map (prefix d . showElement (d + 1)) children
+    in T.intercalate "\n" $ te : ch
+
+showName :: Name -> T.Text
+showName (Name ln ns pr) = fromJust (pr <> return ":" <> return ln )
