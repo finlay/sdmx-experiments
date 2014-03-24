af61795dead436f672d456da736f52becdd3f1dd
Finlay Thompson <finlay@dragonfly.co.nz>
2014-03-24 22:15:43 +1300

    Working better now.

--- read-schema.hs
+++ read-schema.hs
@@ -3,7 +3,7 @@ import System.IO (stdin)
 import Data.Monoid ((<>))
 import Data.Maybe (fromJust, catMaybes)
 
-import Data.Text as T hiding (map)
+import Data.Text as T hiding (map, repeat, take)
 import Data.Text.IO as T
 import Data.Conduit (($$))
 import Data.Conduit.Binary (sourceHandle)
@@ -15,21 +15,24 @@ main = do
     T.putStrLn $ showdoc doc
 
 showdoc :: X.Document -> T.Text
-showdoc doc = showElement 0 $ documentRoot doc
+showdoc doc = 
+    let tops =  anyElement $ fromDocument doc
+    in  T.concat $ catMaybes $ map (showElement 0) tops
 
-rep i = Prelude.take i . Prelude.repeat
 
 type Depth = Int
-showElement :: Depth -> Element -> T.Text
-showElement d el = 
-    let te = showName $ elementName el
-        selectElements (NodeElement el) = Just el
-        selectElements _ = Nothing
-        children = catMaybes $ map selectElements $ elementNodes el
-        prefix :: Int -> Text -> Text
-        prefix d txt = T.concat $ rep d "    " ++ [txt]
-        ch = map (prefix d . showElement (d + 1)) children
-    in T.intercalate "\n" $ te : ch
+showElement :: Depth -> Cursor -> Maybe T.Text
+showElement d cur = do
+    let prefix = return $ T.concat $ take (d+1) $ repeat "    "
+    let children = child >=> anyElement $ cur
+    ch <- mapM ((prefix <>) . showElement (d+1)) children
+    te <- showNode (node cur)
+    return $ T.intercalate "\n" $ te : ch
 
-showName :: Name -> T.Text
-showName (Name ln ns pr) = fromJust (pr <> return ":" <> return ln )
+showNode :: Node -> Maybe T.Text
+showNode (NodeElement el) =
+    let Name ln ns pr = elementName el
+    in  pr <> return ":" <> return ln 
+showNode (NodeContent c) = Just $ "\"" <> c <> "\""
+showNode (NodeComment c) = Just $ "-- " <> c
+showNode _ = Nothing
